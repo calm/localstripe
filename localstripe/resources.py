7d567c7155b999086a647c88cdf5810f6dd8b98b
Maya Li <maya@calm.com>
2022-07-25 17:43:06 -0700

    add assert error messages for bad request and convert percent_off to float

--- localstripe/resources.py
+++ localstripe/resources.py
@@ -547,33 +547,47 @@ class Coupon(StripeObject):
             raise UserError(400, 'Unexpected ' + ', '.join(kwargs.keys()))
 
         amount_off = try_convert_to_int(amount_off)
-        percent_off = try_convert_to_int(percent_off)
+        percent_off = try_convert_to_float(percent_off)
         duration_in_months = try_convert_to_int(duration_in_months)
         try:
-            assert type(id) is str and id
-            assert (amount_off is None) != (percent_off is None)
+            assert type(id) is str and id, \
+                f"id is required and must be a string, got: {id}"
+            assert (amount_off is None) != (percent_off is None), \
+                "either amount_off or percent_off must be defined"
             if name is not None:
-                assert type(name) is str and name
+                assert type(name) is str and name, \
+                    f"name is expected to be a str, got: {name}"
             if amount_off is not None:
-                assert type(amount_off) is int and amount_off >= 0
+                assert type(amount_off) is int and amount_off >= 0, \
+                    f"amount_off expected to be an int greater than 0, got: {amount_off}"
             if percent_off is not None:
                 if type(percent_off) is str:
                     percent_off = float(percent_off)
-                assert type(percent_off) is float or type(percent_off) is int
-                assert percent_off >= 0 and percent_off <= 100
-            assert duration in ('forever', 'once', 'repeating')
+                assert type(percent_off) is float or type(percent_off) is int, \
+                    f"percent_off be of type float or int, got: {type(percent_off)}"
+                assert 0 <= percent_off <= 100, \
+                    f"percent_off must be between 0 and 100, got: {percent_off}"
+            assert duration in ('forever', 'once', 'repeating'), \
+                f"duration must be one of 'forever', 'once', 'repeating', got {duration} "
             if amount_off is not None:
-                assert type(currency) is str and currency
+                assert type(currency) is str and currency, f"currency is expected, got: {currency}"
             if duration == 'repeating':
-                assert type(duration_in_months) is int
-                assert duration_in_months > 0
+                assert type(duration_in_months) is int, \
+                    f"duration_in_months expected to be an int, got: {type(duration_in_months)}"
+                assert duration_in_months > 0, \
+                    f"duration_in_months greater than 0 expected, got: {duration_in_months}"
             if max_redemptions is not None:
-                assert type(max_redemptions) is int
+                assert type(max_redemptions) is int, \
+                    f"max_redemptions expected to be an int, got: {type(max_redemptions)}"
+                assert max_redemptions > 0, \
+                    f"max_redemptions greater than 0 expected, got: {max_redemptions}"
             if redeem_by is not None:
-                assert type(redeem_by) is int
-                assert redeem_by > 0
-        except AssertionError:
-            raise UserError(400, 'Bad request')
+                assert type(redeem_by) is int, \
+                    f"redeem_by expected to be an int, got: {type(redeem_by)}"
+                assert redeem_by > 0,\
+                    f"redeem_by greater than 0 expected, got: {redeem_by}"
+        except AssertionError as assertionErr:
+            raise UserError(400, 'Bad request', {"original_error": assertionErr})
 
         # All exceptions must be raised before this point.
         super().__init__(id)
